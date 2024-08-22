cf63fee08336391984a5acea2b5a4b60e5acef99
Ameut <ameur.ouafi@hotmail.fr>
2024-08-22 23:04:42 +0200

    corection doker

--- liste/static/liste/js/img_habitat.js
+++ liste/static/liste/js/img_habitat.js
@@ -1,14 +1,41 @@
+console.log("img_habitat.js is loaded");
+
 function openImage(imgElement, habitatName) {
+    console.log("openImage called with:", imgElement, habitatName);
     var modal = document.getElementById("imageModal");
     var modalImg = document.getElementById("modalImage");
     var title = document.getElementById("habitatTitle");
 
-    modal.style.display = "flex";
-    modalImg.src = imgElement.src;
-    title.textContent = habitatName;
+    if (modal && modalImg && title) {
+        modal.style.display = "flex";
+        modalImg.src = imgElement.src;
+        title.textContent = habitatName;
+    } else {
+        console.log("Modal elements are not found");
+    }
 }
 
 function closeImage() {
     var modal = document.getElementById("imageModal");
-    modal.style.display = "none";
+    if (modal) {
+        modal.style.display = "none";
+    }
 }
+
+document.querySelectorAll('.image-container').forEach(function(container) {
+    container.addEventListener('mouseover', function() {
+        var img = container.querySelector('.clickable-image');
+        var title = container.querySelector('.habitat-title');
+
+        img.style.transform = 'scale(1.1)';
+        title.style.opacity = '1';
+    });
+
+    container.addEventListener('mouseout', function() {
+        var img = container.querySelector('.clickable-image');
+        var title = container.querySelector('.habitat-title');
+
+        img.style.transform = 'scale(1)';
+        title.style.opacity = '0';
+    });
+});
--- zooArcadia/settings.py
+++ zooArcadia/settings.py
@@ -172,3 +172,5 @@ LOGGING = {
     },
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
